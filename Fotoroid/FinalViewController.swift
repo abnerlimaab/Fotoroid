6d234a4ff0d75bc69041a830c0aeecbde00fe689
Abner Lima <abnerlima.1997@gmail.com>
2023-04-22 09:31:14 -0300

    Preparando o projeto

---
+++ Fotoroid/FinalViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FinalViewController.swift
+//  Fotoroid
+//
+//  Created by Abner Lima on 22/04/23.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+    
+    @IBOutlet weak var ivPhoto: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+    @IBAction func restart(_ sender: UIButton) {
+    }
+    
+
+}
--- Fotoroid/ViewController.swift
+++ Fotoroid/ViewController.swift
@@ -15,5 +15,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func selectSource(_ sender: UIButton) {
+    }
 }
 
