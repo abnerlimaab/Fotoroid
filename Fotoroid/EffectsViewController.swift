9d9f41ccd9d7d1b480bb7b5716bb1810150af4e2
Abner Lima <abnerlima.1997@gmail.com>
2023-04-23 16:16:28 -0300

    Aplicando filtros com CoreImage

--- Fotoroid/EffectsViewController.swift
+++ Fotoroid/EffectsViewController.swift
@@ -14,10 +14,20 @@ class EffectsViewController: UIViewController {
     @IBOutlet weak var ivPhoto: UIImageView!
     
     var image: UIImage!
+    lazy var filterManager: FilterManager = {
+        let filterManager = FilterManager(image: image)
+        return filterManager
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         ivPhoto.image = image
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let filteredImage = filterManager.applyFilter(type: .comic)
+        ivPhoto.image = filteredImage
     }
     
     override func viewWillAppear(_ animated: Bool) {
